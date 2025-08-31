422ca57abb78c6220ab9a013600b7c9d6a6316c0
Arin-Chandrakar <arin23official@gmail.com>
2025-08-31 13:26:46 +0530

    about Me

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,44 @@ import { GridBackgroundDemo } from './components/BackgroundGrid'
 function App() {
   return(
     <Background/>
+    
   )
 }
 
 function Background(){
   return(
     <div>
-      <GridBackgroundDemo/>
+      {/* <GridBackgroundDemo/> */}
+      <NameIntro/>
       <AboutMe/>
+      
+    </div>
+  )
+}
+
+function NameIntro(){
+  return(
+    <div className='bg-pink-400 h-64 w-[900px] mx-auto mt-20 flex flex-row justify-between'>
+      <h1 className='text-5xl pl-5 mt-7 h-1.5'>Hi,How are you</h1>
+      <FaceCircle/>
     </div>
   )
 }
 
 function AboutMe(){
   return(
-    <div className='relative bg-pink-400 h-64 w-[900px] mx-auto mt-20'>
-      <h1 className='text-5xl pl-5 pt-4 h-1.5 '>Hi,How are you</h1>
+    <div className='bg-amber-600 h-48 w-[900px] mx-auto'>
+      Hey
     </div>
   )
+}
+
+function FaceCircle(){
+    return(
+        <div className='bg-blue-600 w-44 h-44 rounded-full mt-7 mr-7 flex items-center justify-center font-bold'>
+          Hi
+        </div>
+    )
 }
 
 export default App
