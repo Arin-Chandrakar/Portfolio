5ea1f3c9713ce75e7e8c654f4c5d345161d0156e
Arin-Chandrakar <arin23official@gmail.com>
2025-09-13 23:34:23 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import {Home} from "./routes/Home"
 // import {Blog} from "./routes/Blog"
-import {BrowserRouter,Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {FloatingDock} from './components/Dock.jsx';
 import { FullBlog } from './routes/BlogInfo.jsx'
 import { BlogTable } from './routes/Blog'
@@ -12,15 +12,13 @@ import { ChessPage } from './routes/Chess.jsx';
 function App() {
   return(
     <>
-      <BrowserRouter> 
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/Blog" element={<BlogTable/>}/>
-            <Route path="/Blog/:slug" element={<FullBlog/>}/>
-            <Route path="/chess" element={<ChessPage/>}/>
-          </Routes>  
-      </BrowserRouter>  
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Home" element={<Home/>}/>
+      <Route path="/Blog" element={<BlogTable/>}/>
+      <Route path="/Blog/:slug" element={<FullBlog/>}/>
+      <Route path="/chess" element={<ChessPage/>}/>
+    </Routes>    
     <FloatingDock  items={[
         { title: "Home", href: "/home", icon: <img src="/home.png" alt="Web" className='rounded-sm filter invert hover:cursor-pointer ' /> },
         { title: "GitHub", href: "https://github.com/", icon: <img src="/github.png" alt="GitHub" className='rounded-sm brightness-1500 hover:cursor-pointer'/> },
